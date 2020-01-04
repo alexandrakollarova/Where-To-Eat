02c6a797cf52c1b7bf0636e3677ae21856026822
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-01-04 12:57:30 -0500

    env vars renamed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     //API_ENDPOINT: 'https://where-to-eat-api.herokuapp.com/api',
     API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: process.env.TOKEN_KEY,
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+    TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
+    GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
 }
